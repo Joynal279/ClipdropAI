a20044e5ff909d3ad708fcbed653945acc332d3b
Joynal Abedin <44470728+Joynal279@users.noreply.github.com>
2024-07-09 11:27:13 +0600

    updated

---
+++ Example/ClipdropAIExample/ClipdropAIExampleApp.swift
@@ -0,0 +1,17 @@
+//
+//  ClipdropAIExampleApp.swift
+//  ClipdropAIExample
+//
+//  Created by Joynal Abedin on 9/7/24.
+//
+
+import SwiftUI
+
+@main
+struct ClipdropAIExampleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
