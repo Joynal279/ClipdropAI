e5a3c21cd4d009b1993d5ee219a14530926b8081
Joynal Abedin <44470728+Joynal279@users.noreply.github.com>
2024-07-10 02:43:23 +0600

    Example code in progress

--- Example/ClipdropAIExample/ContentView.swift
+++ Example/ClipdropAIExample/ContentView.swift
@@ -6,8 +6,15 @@
 //
 
 import SwiftUI
+import ClipdropAI
 
 struct ContentView: View {
+    
+    @State private var apiKey: String = "YOUR_API_KEY_HERE"
+    @State private var processedImage: UIImage?
+    @State private var originalImage: UIImage?
+    @State private var maskImage: UIImage?
+    
     var body: some View {
         VStack {
             Image(systemName: "globe")
@@ -19,6 +26,70 @@ struct ContentView: View {
     }
 }
 
+//MARK: - CLEAN UP IMAGE
+extension ContentView {
+    private func getCleanUpImage(){
+        if let originalImage = originalImage, let maskImage = maskImage {
+            ClipDrop.shared.paintImageRequest(
+                apiKey: apiKey,
+                image: originalImage,
+                mask: maskImage,
+                text: "A woman with a blue color shari")
+            { success, message, imageData in
+                if success { self.processedImage = imageData }
+                else{ print(message as Any) }
+            }
+        }
+    }
+}
+
+//MARK: - IN PAINTING IMAGE
+extension ContentView {
+    
+}
+
+//MARK: - RE-IMAGINE IMAGE
+extension ContentView {
+    
+}
+
+//MARK: - REMOVE BACKGROUND IMAGE
+extension ContentView {
+    
+}
+
+//MARK: - REMOVE TEXT IMAGE
+extension ContentView {
+    
+}
+
+//MARK: - REPLACE BACKGROUND IMAGE
+extension ContentView {
+    
+}
+
+//MARK: - SKETCH TO IMAGE
+extension ContentView {
+    
+}
+
+//MARK: - TEXT TO IMAGE
+extension ContentView {
+    
+}
+
+//MARK: - UN-CROP IMAGE
+extension ContentView {
+    
+}
+
+//MARK: - UP-SCALLING IMAGE
+extension ContentView {
+    
+}
+
+
+//MARK: - PREVIEW
 #Preview {
     ContentView()
 }
