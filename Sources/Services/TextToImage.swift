29c40ae2585b73773e4b066012719e821e96784e
Joynal Abedin <44470728+Joynal279@users.noreply.github.com>
2024-07-09 18:35:12 +0600

    textToImage api implemented

--- Sources/Services/TextToImage.swift
+++ Sources/Services/TextToImage.swift
@@ -1,8 +1,49 @@
 //
 //  File.swift
-//  
+//
 //
 //  Created by Joynal Abedin on 9/7/24.
 //
 
-import Foundation
+import SwiftUI
+import Alamofire
+
+@available(iOS 14.0, *)
+extension ClipDrop {
+    
+    public func sketchToImageRequest(apiKey: String, text: String, completion: @escaping (_ success: Bool, _ message: String?, _ imageData: UIImage?) -> Void) {
+        
+        let textData = text.data(using: .utf8)
+        
+        let headers: HTTPHeaders = [
+            "x-api-key": apiKey
+        ]
+        
+        AF.upload(
+            multipartFormData: { multipartFormData in
+                multipartFormData.append(textData!, withName: "prompt")
+            },
+            to: "https://clipdrop-api.co/text-to-image/v1",
+            headers: headers
+        )
+        .validate()
+        .responseData(queue: .global()) { response in
+            switch response.result {
+            case .success: do {
+                if let dataSafe = response.data, let imageReceived = UIImage.init(data: dataSafe) {
+                    completion(true, String.init(data: dataSafe, encoding: .utf8), imageReceived)
+                } else {
+                    if let dataSafe = response.data {
+                        completion(false, String.init(data: dataSafe, encoding: .utf8), nil)
+                    }
+                }
+            }
+            case .failure(_):
+                if let dataSafe = response.data {
+                    completion(false, String.init(data: dataSafe, encoding: .utf8), nil)
+                }
+            }
+        }
+    }
+    
+}
