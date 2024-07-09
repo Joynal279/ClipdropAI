669bd3a146c8a8e73e4a3c9f69a0e40c6e89a143
Joynal Abedin <44470728+Joynal279@users.noreply.github.com>
2024-07-09 16:30:59 +0600

    Reimagine file added

---
+++ Sources/Services/ReImagine.swift
@@ -0,0 +1,52 @@
+//
+//  File.swift
+//
+//
+//  Created by Joynal Abedin on 9/7/24.
+//
+
+import SwiftUI
+import Alamofire
+
+@available(iOS 14.0, *)
+extension ClipDrop {
+    func reImagineImage(apiKey: String, image: UIImage, completion: @escaping (_ success: Bool, _ message: String?, _ imageData: UIImage?) -> Void) {
+        
+        let imageData = image.jpegData(compressionQuality: 0.9)!
+        
+        let headers: HTTPHeaders = [
+            "x-api-key": apiKey
+        ]
+        
+        AF.upload(
+            multipartFormData: { multipartFormData in
+                multipartFormData.append(
+                    imageData,
+                    withName: "image_file",
+                    fileName: "Appartment.jpg",
+                    mimeType: "image/jpeg"
+                )
+            },
+            to: "https://clipdrop-api.co/reimagine/v1/reimagine",
+            headers: headers
+        )
+        .validate()
+        .responseData(queue: .global()) { response in
+            switch response.result {
+            case .success: do {
+                if let dataSafe = response.data, let imageReceived = UIImage.init(data: dataSafe) {
+                    completion(true, String.init(data: dataSafe, encoding: .utf8), imageReceived)
+                } else {
+                    if let dataSafe = response.data {
+                        completion(false, String.init(data: dataSafe, encoding: .utf8), nil)
+                    }
+                }
+            }
+            case .failure(_):
+                if let dataSafe = response.data {
+                    completion(false, String.init(data: dataSafe, encoding: .utf8), nil)
+                }
+            }
+        }
+    }
+}
