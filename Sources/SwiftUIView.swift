6be56a3a43cd58b803d84ca3c62a87b97038af57
Joynal Abedin <44470728+Joynal279@users.noreply.github.com>
2024-07-09 11:31:32 +0600

    swiftUIView file added

---
+++ Sources/SwiftUIView.swift
@@ -0,0 +1,20 @@
+//
+//  SwiftUIView.swift
+//  
+//
+//  Created by Joynal Abedin on 9/7/24.
+//
+
+import SwiftUI
+
+@available(iOS 13.0, *)
+struct SwiftUIView: View {
+    var body: some View {
+        Text("Clip Drop API Package!")
+    }
+}
+
+@available(iOS 13.0, *)
+#Preview {
+    SwiftUIView()
+}
