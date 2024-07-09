68ba07caa8e816ad01fe798ac9e12a7ca064f2fc
Joynal Abedin <44470728+Joynal279@users.noreply.github.com>
2024-07-09 18:46:30 +0600

    cleanUp api implemented

--- Sources/Services/CleanUpImage.swift
+++ Sources/Services/CleanUpImage.swift
@@ -1,8 +1,62 @@
 //
 //  File.swift
-//  
+//
 //
 //  Created by Joynal Abedin on 9/7/24.
 //
 
-import Foundation
+import SwiftUI
+import Alamofire
+
+@available(iOS 14.0, *)
+extension ClipDrop {
+    
+    func cleanUpImageRequest(apiKey: String, image: UIImage, mask: UIImage, completion: @escaping (_ success: Bool, _ message: String?, _ imageData: UIImage?) -> Void) {
+        
+        let imageData = image.jpegData(compressionQuality: 0.9)!
+        let maskData = mask.jpegData(compressionQuality: 0.9)!
+        
+        let headers: HTTPHeaders = [
+            "x-api-key": apiKey
+        ]
+        
+        AF.upload(
+            multipartFormData: { multipartFormData in
+                multipartFormData.append(
+                    imageData,
+                    withName: "image_file",
+                    fileName: "image.jpg",
+                    mimeType: "image/jpeg"
+                )
+                multipartFormData.append(
+                    maskData,
+                    withName: "mask_file",
+                    fileName: "mask.png",
+                    mimeType: "image/png"
+                )
+            },
+            to: "https://clipdrop-api.co/cleanup/v1",
+            headers: headers
+        )
+        .validate()
+        .responseData(queue: .global()) { response in
+            switch response.result {
+            case .success: do {
+                if let dataSafe = response.data, let imageReceived = UIImage.init(data: dataSafe) {
+                    completion(true, String.init(data: dataSafe, encoding: .utf8), imageReceived)
+                } else {
+                    if let dataSafe = response.data {
+                        completion(false, String.init(data: dataSafe, encoding: .utf8), nil)
+                    }
+                }
+            }
+            case .failure(_):
+                if let dataSafe = response.data {
+                    completion(false, String.init(data: dataSafe, encoding: .utf8), nil)
+                }
+            }
+        }
+    }
+    
+}
+
